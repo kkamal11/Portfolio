a6286a9ec423293d43b06d3c60988b7e52d3ddd3
kkamal11 <89385593+kkamal11@users.noreply.github.com>
2022-09-16 09:22:24 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,9 @@ const footer = {
         <footer class="clear-both p-4 bg-white sm:p-6 dark:bg-gray-900">
             <div class="flex items-center justify-center">
                 <div class="my-8 md:px-36 sm:px-14 text-gray-700">
-                    <a href="https://github.com/kkamal11" class="hover:text-cyan-500 inline-block mr-6"><i class="bi bi-github" style="font-size:30px" target="_blank"></i></a>
-                    <a href="https://www.linkedin.com/in/kamal-kishor-chaurasiya-a22866192" class=" hover:text-cyan-500 inline-block mr-6"><i class="bi bi-linkedin" style="font-size:30px" target="_blank"></i></a>
-                    <a href="mailto:kishorkamal7091@gmail.com" class="hover:text-cyan-500 inline-block"><i class="bi bi-google" style="font-size:30px" target="_blank"></i></a>
+                    <a href="https://github.com/kkamal11" class="hover:text-cyan-500 inline-block mr-6" target="_blank"><i class="bi bi-github" style="font-size:30px"></i></a>
+                    <a href="https://www.linkedin.com/in/kamal-kishor-chaurasiya-a22866192" class=" hover:text-cyan-500 inline-block mr-6" target="_blank"><i class="bi bi-linkedin" style="font-size:30px"></i></a>
+                    <a href="mailto:kishorkamal7091@gmail.com" class="hover:text-cyan-500 inline-block"><i class="bi bi-google" style="font-size:30px"></i></a>
                 </div>
             </div>
             <hr class="mb-4 border-gray-300 sm:mx-auto dark:border-gray-700 " >
