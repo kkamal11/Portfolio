69594db3cc771441e39a33dbbeccce8f3a370ad9
Kamal Kishor <89385593+kkamal11@users.noreply.github.com>
2023-04-08 11:48:15 +0530

    Update github_stats.js

--- components/github_stats.js
+++ components/github_stats.js
@@ -4,7 +4,7 @@ export default {
         <div class="text-center mt-2 p-6 text-gray-700 dark:text-gray-300">
             <p class="uppercase tracking-widest text-xl font-medium">Github Statistics</p>
         </div>
-        <div class="mx-6 sm:mx-0 block lg:flex justify-center item-center">
+        <div class="mx-6 sm:mx-0 block md:flex justify-center item-center">
             <img src="https://github-readme-stats.vercel.app/api?username=kkamal11&show_icons=true" />
             <img src="https://streak-stats.demolab.com/?user=kkamal11" />
         </div>
