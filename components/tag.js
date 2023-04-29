f3f359928fcf5643171266df62bbd9642c73f9e0
Kamal Kishor <89385593+kkamal11@users.noreply.github.com>
2023-04-29 21:28:02 +0530

    Update tag.js

--- components/tag.js
+++ components/tag.js
@@ -1,7 +1,7 @@
 const tag = {
     template: `
     <div class="tag-fade">
-        <span class="rounded-2xl mt-2 mx-2 px-2 py-1 bg-sky-600 inline-block text-xs text-white hover:bg-sky-700 hover:cursor-auto">
+        <span class="rounded mt-2 mx-2 px-2 py-1.5 bg-sky-600 inline-block text-xs text-white hover:bg-sky-700 hover:cursor-auto">
             <slot></slot>
         </span>
     </div>
