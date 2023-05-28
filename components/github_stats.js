d91e4b8d421ac28809ae3c77eec7707faee8e549
Kamal Kishor <89385593+kkamal11@users.noreply.github.com>
2023-05-28 11:57:09 +0530

    Added loader

--- components/github_stats.js
+++ components/github_stats.js
@@ -4,18 +4,26 @@ export default {
         <div class="text-center mt-2 p-6 text-gray-700 dark:text-gray-300">
             <p class="uppercase tracking-widest text-xl font-medium">Github Statistics</p>
         </div>
-        <div class="mx-6 sm:mx-0 block md:flex justify-center item-center">
-            <img v-if="!isDark" src="https://github-readme-stats.vercel.app/api?username=kkamal11&show_icons=true" />
-            <img v-if="isDark" src="https://github-readme-stats.vercel.app/api?username=kkamal11&show_icons=true&theme=radical" />
+        <div class="mx-6 sm:mx-0 block lg:flex justify-center item-center">
+            <div v-if="showLoader" class="text-center">
+                <div class="my-2 text-sm text-gray-500">Fetching From Github...</div>
+                <div class="loader"></div>
+            </div>
+            <div v-else class="lg:flex justify-center item-center">
+                <img v-if="!isDark" src="https://github-readme-stats.vercel.app/api?username=kkamal11&show_icons=true" />
+                <img v-if="isDark" src="https://github-readme-stats.vercel.app/api?username=kkamal11&show_icons=true&theme=radical" />
 
-            <img v-if="!isDark" src="https://streak-stats.demolab.com/?user=kkamal11" />
-            <img v-if="isDark" src="https://streak-stats.demolab.com/?user=kkamal11&theme=radical" />
+                <img v-if="!isDark" src="https://streak-stats.demolab.com/?user=kkamal11" />
+                <img v-if="isDark" src="https://streak-stats.demolab.com/?user=kkamal11&theme=radical" />
+            </div>
         </div>
     </div>
     `,
     data() {
         return {
             isDark: false,
+            showLoader: true,
+            id:null
         }
     },
     methods: {
@@ -26,10 +34,15 @@ export default {
             else {
                 this.isDark = false;
             }
+        },
+        showLoaderFunc() {
+            this.showLoader = !this.showLoader;
+            clearTimeout(this.id);
         }
     },
     mounted() {
         this.checkForDarkMode();
+        this.id = setTimeout(this.showLoaderFunc, 2000);
     },
 
 
