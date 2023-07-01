13f4eeadd27da8b18a274843b54b7f1b6667b1a7
Kamal Kishor <89385593+kkamal11@users.noreply.github.com>
2023-07-01 22:18:27 +0530

    Added visit count

--- components/footer.js
+++ components/footer.js
@@ -25,7 +25,12 @@ const footer = {
             </div>
             <hr class="mb-4 border-gray-300 sm:mx-auto dark:border-gray-700 " >
             <div class="grid sm:grid-cols-3 items-center">
-                <div class="mb-8 sm:mb-0 md:mx-auto text-sm text-gray-500 text-center dark:text-gray-400">Last Update: Jan, 2023</div>
+                <div class="mb-8 sm:mb-0 md:mx-auto text-sm text-gray-500 text-center dark:text-gray-400">
+                    <p>Last Update: Jan, 2023</p>
+                    <div>
+                        <img src="https://visitcount.itsvg.in/api?id=kkamal11&label=Profile%20Views&color=6&icon=0&pretty=true" />
+                    </div>
+                </div>
                 <div class="mb-8 sm:mb-0 md:mx-auto text-sm text-gray-500 text-center dark:text-gray-400">&copy; 2023 Kamal Kishor Chaurasiya</div>
                 <div>
                     <p v-for="[key,value] in Object.entries(links)" class="md:mx-auto text-sm text-gray-500 text-center hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"><router-link :to="value" class="hover:underline underline-offset-2 hover:cursor-pointer">{{ key }}</router-link></p>
